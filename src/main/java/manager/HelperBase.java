85213bf0220814ec4ecb0550b738a3de11f9dada
vitber36 <vitber36@gmail.com>
2024-01-06 20:34:51 +0200

    NegativeLogin

--- src/main/java/manager/HelperBase.java
+++ src/main/java/manager/HelperBase.java
@@ -1,8 +1,11 @@
 package manager;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -46,5 +49,19 @@ public class HelperBase {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public boolean isAlertPresent(String message) {
+        Alert alert = new WebDriverWait(wd,10)
+                .until(ExpectedConditions.alertIsPresent());
+        if (alert!=null&&alert.getText().contains(message)){
+            //click ok-->
+            //pause(5000);
+            alert.accept();
+            //click cancel--> alert.dismiss();
+            //type into alert--> alert.sendKeys("hello");
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -35,8 +35,8 @@ LoginTests extends TestBase{
     @Test
     public void loginSuccess() {
         User user = new User().withEmail("vitber06@mail.ru").withPassword("1978Vit@lik");
-//        user.setEmail("margo@gmail.com");
-//        user.setPassword("Mmar123456$");
+//        user.setEmail("vitber06@mail.ru");
+//        user.setPassword("1978Vit@lik");
         app.getHelperUser().openLoginForm();
         app.getHelperUser().fillLoginForm(user);
         app.getHelperUser().submitLogin();
@@ -59,6 +59,69 @@ LoginTests extends TestBase{
         Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
         //app.getHelperUser().clickOKButton();
 
+
+    }
+
+    @Test
+    public void loginWrongEmailWithoutShtrudel(){
+        User user=new User().withEmail("vitber06mail.ru").withPassword("1978Vit@lik");
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submitLogin();
+        Assert.assertFalse(app.getHelperUser().isLogged());
+    }
+
+    @Test
+    public void loginWrongEmailWithoutShtrudel1(){
+        User user=new User().withEmail("vitber06mail.ru").withPassword("1978Vit@lik");
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm(user);
+        //app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("It'snot look like email"));
+    }
+
+
+    @Test
+
+    public void loginWrongEmailWithoutPoint(){
+        User user=new User().withEmail("vitber06@mailru").withPassword("1978Vit@lik");
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submitLogin();
+        Assert.assertFalse(app.getHelperUser().isLogged());
+
+    }
+
+    @Test
+
+    public void loginWrongPasswordShort(){
+        User user=new User().withEmail("vitber06@mail.ru").withPassword("8Vit@");
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submitLogin();
+        Assert.assertEquals(app.getHelperUser().getMessage(),"Login or Password incorrect");
+
+    }
+
+    @Test
+
+    public void loginWrongPasswordWithoutDigits(){
+        User user=new User().withEmail("vitber06@mail.ru").withPassword("qqqqVit@lik");
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submitLogin();
+        Assert.assertEquals(app.getHelperUser().getMessage(),"Login or Password incorrect");
+
+    }
+
+    @Test
+
+    public void loginWrongPasswordWithoutSpecialSymbol(){
+        User user=new User().withEmail("vitber06@mail.ru").withPassword("1978Vitalik");
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm(user);
+        app.getHelperUser().submitLogin();
+        Assert.assertFalse(app.getHelperUser().isLogged());
 
     }
 
